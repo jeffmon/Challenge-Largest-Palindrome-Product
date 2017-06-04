b63a7d3d51bcf15e01c9872996310685b268a8f3
jeffmon <jeffmon@gmail.com>
2017-06-03 19:49:51 -1000

    finished

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,12 +5,34 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+
+module.exports = function(digit){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  var max;
+
+  if(digit === 2){
+    max = 100;
+  }
+  if(digit === 3){
+    max = 1000;
+  }
+
 
   // do your work here
+  for(var i = 1; i < max; i++){
+    for(var j = 1; j < max; j++){
+      var num1 = i * j;
+      var num2 = num1.toString().split("").reverse().join("");
+
+      if(num1 === Number(num2) && palindromeNumber < num1){
+        factor_0 = i;
+        factor_1 = j;
+        palindromeNumber = num1;
+      }
+    }
+  }
 
 
 
